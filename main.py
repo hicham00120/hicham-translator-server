c1918640a93b32dad1a46d2b4d1f355af8b96d8c
hicham00120 <97860522+hicham00120@users.noreply.github.com>
2026-09-17 22:36:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title="HICHAM TRANSLATOR API",
@@ -12,3 +13,30 @@ def home():
         "status": "online",
         "service": "HICHAM TRANSLATOR"
     }
+
+
+@app.post("/translate")
+async def translate_audio(
+    audio: UploadFile = File(...)
+):
+    try:
+        audio_data = await audio.read()
+
+        return JSONResponse(
+            {
+                "success": True,
+                "filename": audio.filename,
+                "content_type": audio.content_type,
+                "size_bytes": len(audio_data),
+                "message": "تم استقبال الصوت بنجاح"
+            }
+        )
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "success": False,
+                "error": str(e)
+            }
+        )
